1956e6fdf1809b90808c8412438cffdf0f79f18d
Subhadeep Roy <joy@MacBook-Air.local>
2024-03-04 01:17:41 +0000

    Sixth Commit

--- source_code/utils.py
+++ source_code/utils.py
@@ -21,7 +21,7 @@ def save_object(file_path, obj):
         raise CustomException(e,sys)
     
 
-def evaluate_model(X_train,y_train,X_test,y_test):
+def evaluate_model(X_train,y_train,X_test,y_test,models):
     try:
         report={}
         for i in range(len(models)):
